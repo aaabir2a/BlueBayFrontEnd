66bbbad7e388d9fef19d5a580f4c837260d554d9
unknown <aaabir2a@gmail.com>
2025-02-23 17:11:47 +0600

    refactor: simplify FloatingShapes component and introduce LottieWrapper for animation handling

--- components/FloatingShapes.tsx
+++ components/FloatingShapes.tsx
@@ -1,187 +1,118 @@
 "use client";
-import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 import { motion } from "framer-motion";
+import LottieWrapper from "./LottieWrapper";
 
-// Floating shapes component
 export default function FloatingShapes() {
+  const animations = [
+    {
+      src: "/animations/Animation - 1.json",
+      position: { top: "15%", left: "15%" },
+      motion: {
+        animate: { y: [0, 20, 0] },
+        transition: {
+          duration: 3,
+          repeat: Number.POSITIVE_INFINITY,
+          ease: "easeInOut",
+        },
+      },
+    },
+    {
+      src: "/animations/Animation - 2.json",
+      position: { top: "45%", left: "75%" },
+      motion: {
+        animate: { y: [0, -20, 0] },
+        transition: {
+          duration: 4,
+          repeat: Number.POSITIVE_INFINITY,
+          ease: "easeInOut",
+        },
+      },
+    },
+    {
+      src: "/animations/Animation - 3.json",
+      position: { top: "65%", left: "25%" },
+      motion: {
+        animate: { y: [0, 15, 0] },
+        transition: {
+          duration: 3.5,
+          repeat: Number.POSITIVE_INFINITY,
+          ease: "easeInOut",
+        },
+      },
+    },
+    {
+      src: "/animations/Animation - 4.json",
+      position: { top: "25%", left: "65%" },
+      motion: {
+        animate: { y: [0, -15, 0] },
+        transition: {
+          duration: 4.5,
+          repeat: Number.POSITIVE_INFINITY,
+          ease: "easeInOut",
+        },
+      },
+    },
+    {
+      src: "/animations/Animation - 5.json",
+      position: { top: "75%", left: "15%" },
+      motion: {
+        animate: { y: [0, 10, 0] },
+        transition: {
+          duration: 5,
+          repeat: Number.POSITIVE_INFINITY,
+          ease: "easeInOut",
+        },
+      },
+    },
+    {
+      src: "/animations/Animation - 6.json",
+      position: { top: "35%", left: "45%" },
+      motion: {
+        animate: { y: [0, -10, 0] },
+        transition: {
+          duration: 4,
+          repeat: Number.POSITIVE_INFINITY,
+          ease: "easeInOut",
+        },
+      },
+    },
+  ];
+
   return (
     <>
-      {/* Animation 1 */}
-      <motion.div
-        className="absolute opacity-30 w-40 h-40"
-        animate={{
-          x: [0, 50, 0],
-          y: [0, 50, 0],
-          scale: [1, 1.1, 1],
-          rotate: [0, 90, 0],
-        }}
-        transition={{
-          duration: 10,
-          repeat: Number.POSITIVE_INFINITY,
-          repeatType: "mirror",
-          ease: "easeInOut",
-        }}
-        style={{ top: "15%", left: "15%" }}
-      >
-        <DotLottieReact
-          src="/animations/Animation - 1.json"
-          loop
-          autoplay
-          style={{ width: "100%", height: "100%" }}
-        />
-      </motion.div>
+      {animations.map((anim, index) => (
+        <motion.div
+          key={index}
+          className="absolute opacity-30 w-32 h-32"
+          style={anim.position}
+          animate={anim.motion.animate}
+          transition={anim.motion.transition}
+        >
+          <LottieWrapper src={anim.src} />
+        </motion.div>
+      ))}
 
-      {/* Animation 2 */}
-      <motion.div
-        className="absolute opacity-30 w-36 h-36"
-        animate={{
-          x: [0, -40, 0],
-          y: [0, -40, 0],
-          scale: [1, 0.9, 1],
-          rotate: [0, -90, 0],
-        }}
-        transition={{
-          duration: 12,
-          repeat: Number.POSITIVE_INFINITY,
-          repeatType: "mirror",
-          ease: "easeInOut",
-        }}
-        style={{ top: "45%", left: "75%" }}
-      >
-        <DotLottieReact
-          src="/animations/Animation - 2.json"
-          loop
-          autoplay
-          style={{ width: "100%", height: "100%" }}
-        />
-      </motion.div>
-
-      {/* Animation 3 */}
-      <motion.div
-        className="absolute opacity-30 w-44 h-44"
-        animate={{
-          x: [0, 30, 0],
-          y: [0, -30, 0],
-          rotate: [0, 45, 0],
-        }}
-        transition={{
-          duration: 8,
-          repeat: Number.POSITIVE_INFINITY,
-          repeatType: "mirror",
-          ease: "easeInOut",
-        }}
-        style={{ top: "65%", left: "25%" }}
-      >
-        <DotLottieReact
-          src="/animations/Animation - 3.json"
-          loop
-          autoplay
-          style={{ width: "100%", height: "100%" }}
-        />
-      </motion.div>
-
-      {/* Animation 4 */}
-      <motion.div
-        className="absolute opacity-30 w-32 h-32"
-        animate={{
-          x: [0, 60, 0],
-          y: [0, -60, 0],
-          scale: [1, 1.2, 1],
-          rotate: [0, 90, 0],
-        }}
-        transition={{
-          duration: 15,
-          repeat: Number.POSITIVE_INFINITY,
-          repeatType: "mirror",
-          ease: "easeInOut",
-        }}
-        style={{ top: "25%", left: "65%" }}
-      >
-        <DotLottieReact
-          src="/animations/Animation - 4.json"
-          loop
-          autoplay
-          style={{ width: "100%", height: "100%" }}
-        />
-      </motion.div>
-
-      {/* Animation 5 */}
-      <motion.div
-        className="absolute opacity-30 w-40 h-40"
-        animate={{
-          x: [0, -50, 0],
-          y: [0, 50, 0],
-          scale: [1, 0.8, 1],
-          rotate: [0, -45, 0],
-        }}
-        transition={{
-          duration: 14,
-          repeat: Number.POSITIVE_INFINITY,
-          repeatType: "mirror",
-          ease: "easeInOut",
-        }}
-        style={{ top: "75%", left: "15%" }}
-      >
-        <DotLottieReact
-          src="/animations/Animation - 5.json"
-          loop
-          autoplay
-          style={{ width: "100%", height: "100%" }}
-        />
-      </motion.div>
-
-      {/* Animation 6 */}
-      <motion.div
-        className="absolute opacity-30 w-36 h-36"
-        animate={{
-          x: [0, 40, 0],
-          y: [0, -40, 0],
-          scale: [1, 1.1, 1],
-          rotate: [0, 60, 0],
-        }}
-        transition={{
-          duration: 16,
-          repeat: Number.POSITIVE_INFINITY,
-          repeatType: "mirror",
-          ease: "easeInOut",
-        }}
-        style={{ top: "35%", left: "45%" }}
-      >
-        <DotLottieReact
-          src="/animations/Animation - 6.json"
-          loop
-          autoplay
-          style={{ width: "100%", height: "100%" }}
-        />
-      </motion.div>
-
-      {/* Subtle background elements */}
+      {/* Simple decorative elements */}
       <motion.div
         className="absolute w-24 h-24 bg-[#00FF88] rounded-full opacity-10 backdrop-blur-sm"
-        animate={{
-          scale: [1, 1.1, 1],
-        }}
+        style={{ top: "30%", left: "60%" }}
+        animate={{ scale: [1, 1.1, 1] }}
         transition={{
           duration: 4,
           repeat: Number.POSITIVE_INFINITY,
-          repeatType: "reverse",
           ease: "easeInOut",
         }}
-        style={{ top: "30%", left: "60%" }}
       />
 
       <motion.div
         className="absolute w-32 h-32 bg-[#FF0066] rounded-full opacity-10 backdrop-blur-sm"
-        animate={{
-          scale: [1, 0.9, 1],
-        }}
+        style={{ top: "60%", left: "50%" }}
+        animate={{ scale: [1, 0.9, 1] }}
         transition={{
           duration: 5,
           repeat: Number.POSITIVE_INFINITY,
-          repeatType: "reverse",
           ease: "easeInOut",
         }}
-        style={{ top: "60%", left: "50%" }}
       />
     </>
   );
